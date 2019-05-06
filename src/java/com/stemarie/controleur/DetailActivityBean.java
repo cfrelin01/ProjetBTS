73e7afacfc40cdab1d1b05a0d2147ac5d081e121
root <root@Frelin-Debian.lan>
2019-05-06 15:37:36 +0200

    new commit

--- src/java/com/stemarie/controleur/DetailActivityBean.java
+++ src/java/com/stemarie/controleur/DetailActivityBean.java
@@ -44,162 +44,230 @@ public class DetailActivityBean implements Serializable {
 
     //graphique 
     private LineChartModel lineModel;
-
+    private LineChartModel lineModelCad;
+    private LineChartModel lineModelHrm;
     
+
     //injecter le controleur d'activite pour recuperer l'activite en cours
     @Inject
     private ActivityBean activityBean;
-    
-    
-    // private LineChartModel zoomModel;
-//    private CartesianChartModel fillToZero;
-//    
-//    private BarChartModel animatedModel2;
-//    private LineChartModel dateModel;
+
     //lancement automatique lors d'un appel
     @PostConstruct
     public void init() {
-        System.out.println("Merci de générer le graphique :"+activityBean.getListDataActivities());
+        System.out.println("Merci de générer le graphique :" + activityBean.getListDataActivities());
         this.createLineModels();
         //createDateModel();
     }
 
-    //definir les parametres du graphique (legende...)
-    private void createLineModels() {
-        //charger les graphiques dans la base de données
+    //definir les parametres du graphique (legende, abscisse, ordonnée..)
+    private void createLineModels(){
+
+//charger les graphiques dans la base de données
         lineModel = this.initLinearModel();
         lineModel.setTitle("Live Activity");
         lineModel.setLegendPosition("ne");
+        lineModel.setSeriesColors("4E3ED2");
         Axis yAxis = lineModel.getAxis(AxisType.Y);
-//        yAxis.setMin(0);
-//        yAxis.setMax(2000);
         Axis xAxis = lineModel.getAxis(AxisType.X);
-//        xAxis.setMin(0);
-//        xAxis.setMax(600);
+
+        lineModelCad = this.initLinearModelCad();
+        lineModelCad.setTitle("Live Activity");
+        lineModelCad.setLegendPosition("ne");
+        lineModelCad.setSeriesColors("E13A18");
+        Axis yAxis2 = lineModelCad.getAxis(AxisType.Y);
+        Axis xAxis2 = lineModelCad.getAxis(AxisType.X);
+        
+        lineModelHrm = this.initLinearModelHrm();
+        lineModelHrm.setTitle("Live Activity");
+        lineModelHrm.setLegendPosition("ne");
+        lineModelHrm.setSeriesColors("FFFFFF");
+        Axis yAxis3 = lineModelHrm.getAxis(AxisType.Y);
+        Axis xAxis3 = lineModelHrm.getAxis(AxisType.X);
+
     }
 
-    
-    //générer les traits des graphiques
+    //générer les lignes du graphique
     private LineChartModel initLinearModel() {
         LineChartModel model = new LineChartModel();
 
-
-        //créer le trait des watts PWR
+        //créer le trait des watts (PWR)
         LineChartSeries pwr = new LineChartSeries();
         pwr.setLabel("Power");
-        
-        //remplir le graphique
-        List<DataActivity> listeDataActivities=this.activityBean.getListDataActivities();
-        
-//    pwr.set("2014-01-01 00:10:50", 51);
-//    pwr.set("2014-01-01 00:10:51", 22);
-//    pwr.set("2014-01-01 00:10:52", 65);
-//    pwr.set("2014-01-01 00:10:53", 35);
-        
-    SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    // myDate is the java.util.Date in yyyy-mm-dd format
 
-        String minAxis="";
-        String maxAxis="";
-        int compteur=0;
-        
+       
+
+        //remplir le graphique
+        List<DataActivity> listeDataActivities = this.activityBean.getListDataActivities();
+
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // myDate is the java.util.Date in yyyy-mm-dd format
+
+        String minAxis = "";
+        String maxAxis = "";
+        int compteur = 0;
+
         //parcourir les données de l'activité
-        for(DataActivity data:listeDataActivities){
+        for (DataActivity data : listeDataActivities) {
             pwr.set(sm.format(data.getTimeData()), data.getPwr());
+        
             //conserver le min
-            if(compteur==0){
-             minAxis=sm.format(data.getTimeData());   
+            if (compteur == 0) {
+                minAxis = sm.format(data.getTimeData());
             }
-            maxAxis=sm.format(data.getTimeData());
+            maxAxis = sm.format(data.getTimeData());
             //System.out.println("Ajouter le point : "+sm.format(data.getTimeData()));
             compteur++;
         }
         //System.out.println("Max axis : "+maxAxis);
-        
 
-
-         model.addSeries(pwr);
-//        model.addSeries(CAD);
-//        model.addSeries(HRM);
-
+        model.addSeries(pwr);
+     
 
         //activer le clic droit pour zoomer
         model.setTitle("Zoom for Details");
         model.setZoom(true);
         model.getAxis(AxisType.Y).setLabel("Values");
 
-        
-        //format date 
+        //format de la date 
         DateAxis axis = new DateAxis("Time");
         axis.setTickAngle(-70);
         //axis.set
         axis.setMin(minAxis);
         axis.setMax(maxAxis);
-        
-        
+
         axis.setTickFormat("%d-%m-%y %H:%#M:%S");
         model.getAxes().put(AxisType.X, axis);
-        
-        
+
         //retourner l'objet modele terminé
         return model;
     }
     
+    //générer les lignes du graphique
+    private LineChartModel initLinearModelCad() {
+        LineChartModel model = new LineChartModel();
+
+        //créer le trait des watts (PWR)
+      
+        //créer le trait cadence(CAD)
+        LineChartSeries cad = new LineChartSeries();
+        cad.setLabel("Cadence");
+
+      
+
+        //remplir le graphique
+        List<DataActivity> listeDataActivities = this.activityBean.getListDataActivities();
+
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // myDate is the java.util.Date in yyyy-mm-dd format
+
+        String minAxis = "";
+        String maxAxis = "";
+        int compteur = 0;
+
+        //parcourir les données de l'activité
+        for (DataActivity data : listeDataActivities) {
+            
+            cad.set(sm.format(data.getTimeData()), data.getCad());
+            
+            //conserver le min
+            if (compteur == 0) {
+                minAxis = sm.format(data.getTimeData());
+            }
+            maxAxis = sm.format(data.getTimeData());
+            //System.out.println("Ajouter le point : "+sm.format(data.getTimeData()));
+            compteur++;
+        }
+        //System.out.println("Max axis : "+maxAxis);
+
+        
+        model.addSeries(cad);
+       
+
+        //activer le clic droit pour zoomer
+        model.setTitle("Zoom for Details");
+        model.setZoom(true);
+        model.getAxis(AxisType.Y).setLabel("Values");
+
+        //format de la date 
+        DateAxis axis = new DateAxis("Time");
+        axis.setTickAngle(-70);
+        //axis.set
+        axis.setMin(minAxis);
+        axis.setMax(maxAxis);
+
+        axis.setTickFormat("%d-%m-%y %H:%#M:%S");
+        model.getAxes().put(AxisType.X, axis);
+
+        //retourner l'objet modele terminé
+        return model;
+    }
+    
+    //générer les lignes du graphique
+    private LineChartModel initLinearModelHrm() {
+        LineChartModel model = new LineChartModel();
 
     
-    
-    
+        //créer le trait cradiaque (HRM)
+        LineChartSeries hrm = new LineChartSeries();
+        hrm.setLabel("HRM");
+
+        //remplir le graphique
+        List<DataActivity> listeDataActivities = this.activityBean.getListDataActivities();
+
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // myDate is the java.util.Date in yyyy-mm-dd format
+
+        String minAxis = "";
+        String maxAxis = "";
+        int compteur = 0;
+
+        //parcourir les données de l'activité
+        for (DataActivity data : listeDataActivities) {
+            
+            
+            hrm.set(sm.format(data.getTimeData()), data.getHrm());
+            //conserver le min
+            if (compteur == 0) {
+                minAxis = sm.format(data.getTimeData());
+            }
+            maxAxis = sm.format(data.getTimeData());
+            //System.out.println("Ajouter le point : "+sm.format(data.getTimeData()));
+            compteur++;
+        }
+        //System.out.println("Max axis : "+maxAxis);
+
+        
+        
+        model.addSeries(hrm);
+
+        //activer le clic droit pour zoomer
+        model.setTitle("Zoom for Details");
+        model.setZoom(true);
+        model.getAxis(AxisType.Y).setLabel("Values");
+
+        //format de la date 
+        DateAxis axis = new DateAxis("Time");
+        axis.setTickAngle(-70);
+        //axis.set
+        axis.setMin(minAxis);
+        axis.setMax(maxAxis);
+
+        axis.setTickFormat("%d-%m-%y %H:%#M:%S");
+        model.getAxes().put(AxisType.X, axis);
+
+        //retourner l'objet modele terminé
+        return model;
+    }
+
     //recupérer les data pour l'activité courante BackingBean
     public List<DataActivity> getListDataActivities() {
         Query query = em.createQuery("SELECT da FROM DataActivity da WHERE da.activity=:activity");
         query.setParameter("activity", this.activity);
         return query.getResultList();
     }
-    
-    
-    
-   
-    
-    
 
-//    public void itemSelect(ItemSelectEvent event) {
-//        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
-//                "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
-//
-//        FacesContext.getCurrentInstance().addMessage(null, msg);
-//    }
-
-//    public LineChartModel getZoomModel() {
-//        return zoomModel;
-//    }
-//
-//    public CartesianChartModel getFillToZero() {
-//        return fillToZero;
-//    }
-//
-//    public LineChartModel getAnimatedModel1() {
-//        return animatedModel1;
-//    }
-//
-//    public BarChartModel getAnimatedModel2() {
-//        return animatedModel2;
-//    }
-//
-//    public LineChartModel getDateModel() {
-//        return dateModel;
-//    }
-    /*private void createFillToZero() {
-        fillToZero = new CartesianChartModel();
- 
-        LineChartSeries series1 = new LineChartSeries();
-        series1.setLabel("Series 1");
- 
-        series1.set("4, -3, 3, 6, 2, -2", 0);
- 
-        fillToZero.addSeries(series1);
-   
-    private static final int NAME = 0x320;*/
-
+    // getter et setter 
     public LineChartModel getLineModel() {
         this.createLineModels();
         return lineModel;
@@ -209,6 +277,24 @@ public class DetailActivityBean implements Serializable {
         this.lineModel = lineModel;
     }
 
+    public LineChartModel getLineModelCad() {
+        return lineModelCad;
+    }
+
+    public void setLineModelCad(LineChartModel lineModelCad) {
+        this.lineModelCad = lineModelCad;
+    }
+
+    public LineChartModel getLineModelHrm() {
+        return lineModelHrm;
+    }
+
+    public void setLineModelHrm(LineChartModel lineModelHrm) {
+        this.lineModelHrm = lineModelHrm;
+    }
+
+ 
+
     public ActivityBean getActivityBean() {
         return activityBean;
     }
@@ -216,5 +302,5 @@ public class DetailActivityBean implements Serializable {
     public void setActivityBean(ActivityBean activityBean) {
         this.activityBean = activityBean;
     }
-    
+
 }
